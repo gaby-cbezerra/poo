db186338d534569634d0a57c7014c215bb1d3c68
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-11-26 17:42:56 -0300

    Criando uma pessoa
    Revisão e uso de construtores numa classe.

--- Personagem.cs
+++ Personagem.cs
@@ -2,38 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace poo
 {
-    public class Personagem
+    public class Pessoa
     {
         public string Nome {get; set;}
-        public int Vida {get; set;}
-
-        public int Ataque {get; set;}
-
-        public int Defesa { get; set;}
-
-        public Personagem( string nome, int vida, int ataque, int defesa){
+        public int Age {get; set;}
+        public Pessoa ( string nome, int age)
+        {
             Nome = nome;
-            Vida = vida;
-            Ataque = ataque;
-            Defesa = defesa;
+            Age = age;
         }
-        public void Atacar(Personagem alvo){
-           int dano = Math.Max(Ataque - alvo.Defesa, 0);
-            alvo.Vida -= dano;
-
-            Console.WriteLine($"{Nome} atacou {alvo.Nome}");
-
-            if ( alvo.Vida <= 0){
-                Console.WriteLine("O alvo foi derrotado");
-            }
-            else{
-                Console.WriteLine("Você não derrotou o alvo");
-            }
-           
+        public Pessoa ()
+        {
+            Nome = "Sem nome";
+            Age = 0;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Personagem person1 = new Personagem("Gab", 200 , 50, 100);
-        Personagem person2 = new Personagem("Dio", 150 , 60, 50);
-        person1.Atacar(person2);
+        Pessoa person1 = new Pessoa("Chayenne" , 12);
+        Console.WriteLine(person1.Nome+ " idade: "+ person1.Age);
 
+          Pessoa person2 = new Pessoa ("Igor", 56);
+        Console.WriteLine(person2.Nome);
+         Console.WriteLine(person2.Age);
 
     }
 }
